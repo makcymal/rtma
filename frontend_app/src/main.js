2f4df037f57de91084d3b6c85e562ad2aad5db18
Gennadiy Karpov <asgardkarpov@yandex.ru>
2024-03-19 00:05:10 +1000

    connected frontend and backend sign up

--- frontend_app/src/main.js
+++ frontend_app/src/main.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.css"
 import router from './router'
+import axios from 'axios'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.baseURL = "http://127.0.0.1:8082/";
 
 createApp(App).use(router).mount('#app')
 
